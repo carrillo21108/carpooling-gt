d26692ff159eb237e22e8635335a15da36c9a81d
Adrian R <rod21691@uvg.edu.gt>
2021-10-20 18:01:52 -0600

    creación de controladores

---
+++ src/org/carpooling/controller/MenuConductorController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.carpooling.controller;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import org.carpooling.sistema.Principal;
+/**
+ *
+ * @author Usuario
+ */
+public class MenuConductorController implements Initializable {
+    
+    @FXML private Button cancelar;
+    private Principal escenarioPrincipal;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+    public Principal getEscenarioPrincipal() {
+        return escenarioPrincipal;
+    }
+
+    public void setEscenarioPrincipal(Principal escenarioPrincipal) {
+        this.escenarioPrincipal = escenarioPrincipal;
+    }   
+    public void obtenerPasajeros(){
+        
+    
+    }
+}
---
+++ src/org/carpooling/controller/PagoPasajeroController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.carpooling.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import org.carpooling.sistema.Principal;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PagoPasajeroController implements Initializable {
+    
+    @FXML private TableColumn pago;
+    private Principal escenarioPrincipal;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+    public Principal getEscenarioPrincipal() {
+        return escenarioPrincipal;
+    }
+
+    public void setEscenarioPrincipal(Principal escenarioPrincipal) {
+        this.escenarioPrincipal = escenarioPrincipal;
+    }
+    
+    public void realizarPago(){
+        
+    }
+}
