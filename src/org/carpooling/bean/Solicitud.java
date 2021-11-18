ca683fb13424a367e5a38be54da5aef2f3fee87e
carrillo21108 <car21108@uvg.edu.gt>
2021-11-18 08:14:13 -0600

    Modelo solicitud

--- src/org/carpooling/bean/Solicitud.java
+++ src/org/carpooling/bean/Solicitud.java
@@ -11,16 +11,66 @@ public class Solicitud {
     private int codigoPasajero;
     private String nombre;
     private String apellidos;
+    private String ubicacion;
     private int codigoConductor;
 
-    public Solicitud(int codigoSolicitud, int codigoPasajero, String nombre, String apellidos, int codigoConductor) {
+    public Solicitud(int codigoSolicitud, int codigoPasajero, String nombre, String apellidos, String ubicacion, int codigoConductor) {
         this.codigoSolicitud = codigoSolicitud;
         this.codigoPasajero = codigoPasajero;
         this.nombre = nombre;
         this.apellidos = apellidos;
+        this.ubicacion = ubicacion;
         this.codigoConductor = codigoConductor;
     }
 
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
     public Solicitud() {
+    }
+
+    public int getCodigoSolicitud() {
+        return codigoSolicitud;
+    }
+
+    public void setCodigoSolicitud(int codigoSolicitud) {
+        this.codigoSolicitud = codigoSolicitud;
+    }
+
+    public int getCodigoPasajero() {
+        return codigoPasajero;
+    }
+
+    public void setCodigoPasajero(int codigoPasajero) {
+        this.codigoPasajero = codigoPasajero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getCodigoConductor() {
+        return codigoConductor;
+    }
+
+    public void setCodigoConductor(int codigoConductor) {
+        this.codigoConductor = codigoConductor;
     }
 }
