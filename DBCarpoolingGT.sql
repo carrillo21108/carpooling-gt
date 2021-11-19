ee048ea598314e8f7230de328328fdafab714734
lop21666 <91706525+lop21666@users.noreply.github.com>
2021-11-18 22:22:19 -0600

    Actualización registro controller

--- DBCarpoolingGT.sql
+++ DBCarpoolingGT.sql
@@ -22,7 +22,7 @@ CREATE TABLE Conductores (
     correo varchar(10) not null,
     usuario varchar(10) not null,
     contrasenia varchar(10) not null,
-    codigoCarro int not null auto_increment,
+    codigoCarro int not null,
     espaciosDisponibles int not null,
     ubicacion varchar(50) not null,
     destino varchar(50) not null,
@@ -124,10 +124,10 @@ Delimiter ;
 -- Procedimientos tabla Conductores
 Delimiter $$
 CREATE PROCEDURE sp_AgregarConductor(IN nombre varchar(10), IN apellidos varchar(10), IN correo varchar(10), IN usuario varchar(10), IN contrasenia varchar(10), 
-										IN espaciosDisponibles int, IN ubicacion varchar(50), IN destino varchar(50))
+										IN codigoCarro int, IN espaciosDisponibles int, IN ubicacion varchar(50), IN destino varchar(50))
 BEGIN
-	INSERT INTO Conductores (nombre, apellidos, correo, usuario, contrasenia , espaciosDisponibles, ubicacion, destino)
-		VALUES(nombre, apellidos, correo, usuario, contrasenia, espaciosDisponibles, ubicacion, destino);
+	INSERT INTO Conductores (nombre, apellidos, correo, usuario, contrasenia , codigoCarro, espaciosDisponibles, ubicacion, destino)
+		VALUES(nombre, apellidos, correo, usuario, contrasenia, codigoCarro, espaciosDisponibles, ubicacion, destino);
 END$$
 Delimiter ;
 
@@ -392,3 +392,21 @@ BEGIN
 
 END$$
 Delimiter ;
+
+-- Procedimiento obtener ultimo carro
+Delimiter $$
+CREATE PROCEDURE sp_obtenerCarro()
+BEGIN 
+	SELECT *
+		FROM Carros ORDER BY codigoCarro ASC LIMIT 1;
+END$$
+Delimiter ;
+
+-- Procedimiento Expulsar Pasajero
+Delimiter $$
+CREATE PROCEDURE sp_ExpulsarPasajero(IN codigoP int)
+BEGIN 
+    UPDATE Pasajeros SET codigoConductor=null WHERE codigoPasajero=codigoP;
+END$$
+Delimiter ;
+
